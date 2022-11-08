3983bfc2c4dbe6e9cec651ff077aac8ea5900954
Touyet <touyette29@gmail.com>
2022-11-08 22:12:21 +0100

    create main page layout and serve js and css dynamically

--- src/server/server.ts
+++ src/server/server.ts
@@ -6,15 +6,15 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "../client"));
-app.use('/static', express.static(path.join(__dirname, "../client/bundled"), { extensions: ["js"] }));
+app.use('/js', express.static(path.join(__dirname, "../client/bundled"), { extensions: ["js"] }));
 app.use('/css', express.static(path.join(__dirname, "../client/styles")));
 app.get('/favicon.ico', (req, res) => { });
 app.get('/', (req, res) => {
-    res.render("pages/index");
+    res.render("pages/index", { script: ["index"], css: ["index"] });
 
 });
 app.get('/*', (req, res) => {
-    res.render("pages" + req.originalUrl);
+    res.render("pages" + req.originalUrl, { script: [req.originalUrl], css: [req.originalUrl] });
 });
 
 
